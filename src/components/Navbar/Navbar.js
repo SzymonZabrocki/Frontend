e042b37e7d3bfec17b2673ab918da6ee298a02a1
SzymonZabrocki <szymonzabrocki98@gmail.com>
2021-01-28 19:03:49 +0100

    modal added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Modal from '../Modal/Modal'
+import AddMeme from '../AddMeme/AddMeme'
 
-const navbar = (props) => {
+class Navbar extends Component {
+  state={
+    purchasing: false
+  }
 
-  
+  purchaseCancleHandler = () => {
+    this.setState({ purchasing: false })
+  }
 
-  return (
-    <nav className="navbar fixed-top navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a href="/" className="navbar-brand">Memownia</a>
-        <form className="d-flex">
-          <a className="navbar-brand" href="/">Dodaj Mema</a>
-          <input className="form-control me-2" type="search" placeholder="Wyszukaj mema" aria-label="Search" />
-          <button className="btn btn-light" type="submit">Szukaj</button>
-        </form>
-      </div>
-    </nav>
+  purchaseContinueHandler = () => {
+    this.setState({purchasing: true})
+  }
+
+  render(){
+    return (
+    <div>
+      <nav className="navbar fixed-top navbar-dark bg-dark">
+        <div className="container-fluid">
+          <a href="/" className="navbar-brand">Memownia</a>
+          <form className="d-flex">
+            <a className="navbar-brand" onClick={this.purchaseContinueHandler}>Dodaj Mema</a>
+            <input className="form-control me-2" type="search" placeholder="Wyszukaj mema" aria-label="Search" />
+            <button className="btn btn-light" type="submit">Szukaj</button>
+          </form>
+        </div>
+      </nav>
+      <Modal show={this.state.purchasing} modalClosed={this.purchaseCancleHandler}>
+        <AddMeme/>
+      </Modal>
+    </div>
   )
+  }
+  
 }
 
-export default navbar
+export default Navbar
--- src/containers/MainPage/MainPage.js
+++ src/containers/MainPage/MainPage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Meme from '../../components/Meme/Meme'
+import Modal from '../../components/Modal/Modal'
+import Addmeme from '../../components/AddMeme/AddMeme'
 
 class MainPage extends Component {
   state = {
     memes: [],
-    error: false
+    error: false,
+    purchasing: false
   }
 
   componentDidMount() {
@@ -20,18 +23,29 @@ class MainPage extends Component {
       })
   }
 
+  purchaseCancleHandler = () => {
+    this.setState({ purchasing: false })
+  }
+
+  purchaseContinueHandler = () => {
+    alert('You Continue')
+  }
+
   render() {
 
     let memes = <p style={{ textAlign: 'center' }}>Something went wrong</p>
     if (!this.state.error) {
       memes = this.state.memes.map(meme => {
-        return <Meme  key={meme._id} title={meme.title} tags={meme.tags} image={meme.memeImage} />
+        return <Meme key={meme._id} title={meme.title} tags={meme.tags} image={meme.memeImage} />
       })
     }
 
     return (
-      <div className="container-md" style={{width: "50%", marginTop: "70px"}}>
+      <div className="container-md" style={{ width: "50%", marginTop: "70px" }}>
         <section >
+          {/* <Modal show={this.state.purchasing} modalClosed={this.purchaseCancleHandler}>
+            <Addmeme/>
+          </Modal> */}
           {memes}
         </section>
       </div>
